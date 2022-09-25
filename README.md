da57feeab8557346b88f1c6e67c30daa161864a2
Aed3r <xxgoffi22xx@gmail.com>
2022-09-25 14:21:20 +0200

    switch to Python

--- README.md
+++ README.md
@@ -1,29 +1,14 @@
 # Multimedia Retrieval Assignment
 
 ## Installation & Setup
- - Run `wsl --install` in a Administrator Terminal (right click on Powershell to 'Run as Administrator') to install WSL 2
- - Install Docker: https://www.docker.com/products/docker-desktop/ and run Docker Desktop
- - Install vscode: https://code.visualstudio.com/
- - Install the Remote Development extension: https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.vscode-remote-extensionpack
- - Install VcXsrv Windows X Server: https://sourceforge.net/projects/vcxsrv/files/latest/download
- - Inside vscode, hit F1 and run the command `Remote-Containers: Open Folder in Container..` and select the "MultimediaRetrieval" folder in the selection screen
- - The container should start up, click on the message in the bottom left corner to see the progression and potential errors
- - In the meantime, run XLaunch, select "One large window", and go through all steps by leaving everything default
- - A big black window should appear, you can reduce its size
- - When the container is done building in vscode, open a terminal if there isn't one open already: 'Terminal' > 'New Terminal' at the top of the screen
- - Run following command: `mkdir -p build; cd build`
- - To generate the project do: `cmake ..`
- - To build the project do: `make`
- - To run the project do: `./main`
- - If everything worked correctly, the XLaunch window should show the libigl program
- - After any changes to the files re-run `make` to see the changes. After any project structure changes (e.g. new files), re-run `cmake ..`
- - Git should work as expected in the container, and all vscode extensions copy over. Here are a few convenient ones for C++ development:
-    - https://marketplace.visualstudio.com/items?itemName=jeff-hykin.better-cpp-syntax
-    - https://marketplace.visualstudio.com/items?itemName=ms-vscode.cpptools
-    - https://marketplace.visualstudio.com/items?itemName=ms-vscode.cpptools-extension-pack
-    - https://marketplace.visualstudio.com/items?itemName=tonybaloney.vscode-pets
+ - Install Python 3.9.13: https://www.python.org/ftp/python/3.9.13/python-3.9.13-amd64.exe - Make sure to select 'Add Python to Path' in the installer!
+ - Open a new terminal and run `pip3 install open3d`
+ - Run `python test.py` to check that everything works
 
 ## Datasets
  - The datasets should be unzipped into the data directory:
    - The Princeton shape benchmark (https://web.archive.org/web/20190323023058/http://shape.cs.princeton.edu/benchmark/download/psb_v1.zip) to ./data/psb_v1 such that ./data/psb_v1/benchmark exists
    - The Labeled PSB dataset (https://people.cs.umass.edu/~kalo/papers/LabelMeshes/labeledDb.7z) to ./data/LabeledDB_new such that ./data/LabeledDB_new/Airplane exists
+
+## Modules
+ - Run `python load_meshes.py` to load a sample file and visualize it. Run `python load_meshes.py <path>` to visualize the OFF/PLY file located at `<path>` 
---
+++ load_meshes.py
@@ -0,0 +1,166 @@
+"""Load a mesh from an OFF file.
+"""
+def load_OFF (filename):
+    lineCount = 0
+    vertexCount = 0
+    faceCount = 0
+    readCounts = False
+    readVertices = False
+    vertices = []
+    faces = []
+
+    with open(filename, 'r') as f:
+        for line in f:
+            if line[0] == '#' or not line.strip() or (lineCount == 0 and line.strip() == 'OFF'):
+                continue
+
+            # Separate line into tokens
+            tokens = line.split()
+
+            if len(tokens) == 3:
+                if not readCounts:
+                    numVerts = int(tokens[0])
+                    numFaces = int(tokens[1])
+                    numEdges = int(tokens[2])
+
+                    vertices = [None] * numVerts
+                    faces = [None] * numFaces
+
+                    readCounts = True
+                    continue
+
+                if readVertices:
+                    raise Exception('Unexpected vertex line in faces section')
+
+                if vertexCount >= numVerts:
+                    raise Exception('More vertices in file than expected!')
+
+                # Read vertex coordinates
+                vertices[vertexCount] = [float(tokens[0]), float(tokens[1]), float(tokens[2])]
+                vertexCount += 1
+                continue
+
+            if len(tokens) == 4:
+                if not readVertices:
+                    readVertices = True
+
+                if faceCount >= numFaces:
+                    raise Exception('More faces in file than expected!')
+
+                if not readCounts:
+                    raise Exception('File line counts missing!')
+
+                if int(tokens[0]) == 3:
+                    # Read triangle 
+                    faces[faceCount] = [int(tokens[1]), int(tokens[2]), int(tokens[3])]
+                elif int(tokens[0]) == 4:
+                    # Read quad
+                    faces[faceCount] = [int(tokens[1]), int(tokens[2]), int(tokens[3])]
+                    faces[faceCount + 1] = [int(tokens[1]), int(tokens[3]), int(tokens[4])]
+                    faceCount += 1
+                else:
+                    raise Exception('Unsupported face type!')
+                
+                faceCount += 1
+                continue
+
+            raise Exception('Unexpected line in file!')
+
+    return vertices, faces
+
+"""Load a mesh from an PLY file.
+"""
+def load_PLY (filename):
+    lineCount = 0
+    vertexCount = 0
+    faceCount = 0
+    readVertices = False
+    vertices = []
+    faces = []
+
+    with open(filename, 'r') as f:
+        for line in f:
+            if line[0:7] == "comment" or not line.strip() or (lineCount == 0 and line.strip() == 'ply') or line.strip() == 'end_header' or line[0:8] == 'property':
+                continue
+
+            # Detect format line
+            if line[0:6] == 'format':
+                if line[7:12] != 'ascii':
+                    raise Exception('Unsupported file format!')
+                continue
+
+            # Detect element line
+            if line[0:7] == 'element':
+                tokens = line.split()
+                if tokens[1] == 'vertex':
+                    numVerts = int(tokens[2])
+                    vertices = [None] * numVerts
+                elif tokens[1] == 'face':
+                    numFaces = int(tokens[2])
+                    faces = [None] * numFaces
+                continue
+
+            # Separate line into tokens
+            tokens = line.split()
+
+            if len(tokens) == 3:
+                if readVertices:
+                    raise Exception('Unexpected vertex line in faces section')
+
+                if vertexCount >= numVerts:
+                    raise Exception('More vertices in file than expected!')
+
+                # Read vertex coordinates
+                vertices[vertexCount] = [float(tokens[0]), float(tokens[1]), float(tokens[2])]
+                vertexCount += 1
+                continue
+
+            if len(tokens) == 4:
+                if not readVertices:
+                    readVertices = True
+
+                if faceCount >= numFaces:
+                    raise Exception('More faces in file than expected!')
+
+                if int(tokens[0]) == 3:
+                    # Read triangle 
+                    faces[faceCount] = [int(tokens[1]), int(tokens[2]), int(tokens[3])]
+                elif int(tokens[0]) == 4:
+                    # Read quad
+                    faces[faceCount] = [int(tokens[1]), int(tokens[2]), int(tokens[3])]
+                    faces[faceCount + 1] = [int(tokens[1]), int(tokens[3]), int(tokens[4])]
+                    faceCount += 1
+                else:
+                    raise Exception('Unsupported face type!')
+                
+                faceCount += 1
+                continue
+
+            raise Exception('Unexpected line in file!')
+
+    return vertices, faces
+
+
+if __name__ == '__main__':
+    import sys
+    import numpy as np
+    import open3d as o3d
+
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        #filename = 'data/LabeledDB_new/Airplane/61.off'
+        filename = 'data/ply/airplane.ply'
+
+    if filename[-4:] == '.ply':
+        vertices, faces = load_PLY(filename)
+    elif filename[-4:] == '.off':
+        vertices, faces = load_OFF(filename)
+    else:
+        raise Exception('Unsupported file format!')
+
+    mesh = o3d.geometry.TriangleMesh()
+    mesh.vertices = o3d.utility.Vector3dVector(vertices)
+    mesh.triangles = o3d.utility.Vector3iVector(faces)
+    mesh.paint_uniform_color([1, 0.706, 0])
+    o3d.visualization.draw_geometries([mesh])
