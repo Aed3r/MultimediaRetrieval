72f40b82ddf7f0f85cc0d677eb88c6b85c41b261
TheLissandra1 <n.qiao@students.uu.nl>
2022-10-16 00:07:00 +0200

    cube root of volume of tetrahedron 4 vertices

--- util.py
+++ util.py
@@ -96,6 +96,16 @@ def triangle_area(v1, v2, v3):
     s = (a + b + c) / 2
     return np.sqrt(s * (s - a) * (s - b) * (s - c))
 
+# Returns the cube root of volume of tetrahedron given by 4 random vertices
+def tetrahedron_volume(v1, v2, v3, v4):
+    # assume the peak point is v4
+    a = v1 - v4
+    b = v2 - v4
+    c = v3 - v4
+    mixed_product = np.dot(a, np.cross(b, c))
+    volume = (np.linalg.norm(mixed_product))/6
+    return np.cbrt(volume)
+
 if __name__ == "__main__":
     import load_meshes
 
