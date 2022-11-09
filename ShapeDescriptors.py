normalization fixes

ShapeDescriptors.py
@@ -14,9 +14,9 @@ FEATUREPLOTSPATH = "data/featurePlots/"
 
 # Calculate the distance between barycenter and origin
 def get_barycenter_origin_distance(mesh):
-    barycenter = get_shape_barycenter(mesh)
+    barycenter = util.get_shape_barycenter(mesh)
     origin = np.asarray([0, 0, 0])
-    barycenter_origin_distance = distance_between(barycenter, origin)
+    barycenter_origin_distance = util.distance_between(barycenter, origin)
 
     return barycenter_origin_distance
 
@@ -24,8 +24,8 @@ def get_barycenter_origin_distance(mesh):
 # Calculate the absolute cosine similarity between 2 vectors
 def get_Cosine_similarity(mesh):
 
-    eigenvalues, eigenvectors = compute_PCA(mesh)
-    largest_eigenvector = eigenvectors[:, np.argmax(eigenvalues)]
+    eigenvalues, eigenvectors = util.compute_PCA(mesh)
+    largest_eigenvector = eigenvectors[np.argmax(eigenvalues)]
     vec1 = largest_eigenvector
     # v2: x-axis
     vec2 = [1, 0, 0]
@@ -362,25 +362,25 @@ if __name__ == '__main__':
     
     for i in range(len(data)):
         # barycenter_origin_distance.append(util.get_barycenter_origin_distance(data[i]))
-        absolute_cosine_similarity.append(util.get_Cosine_similarity(data[i]))
-        longest_AABB_edge.append(util.get_longest_AABB_edge(data[i]))
+        #absolute_cosine_similarity.append(get_Cosine_similarity(data[i]))
+        longest_AABB_edge.append(get_longest_AABB_edge(data[i]))
         
 
-    for i in range(len(data)):
-        print("The %dth origin data feature: " %(i+1))
-        print("barycenter_origin_distance: %.5f" %barycenter_origin_distance[i])
-        print("absolute_cosine_similarity: %.5f" %absolute_cosine_similarity[i])
-        print("AABB_edge_0: %.5f" %longest_AABB_edge[i])
+    #for i in range(len(data)):
+        #print("The %dth origin data feature: " %(i+1))
+        #print("barycenter_origin_distance: %.5f" %barycenter_origin_distance[i])
+        #print("absolute_cosine_similarity: %.5f" %absolute_cosine_similarity[i])
+        #print("AABB_edge_0: %.5f" %longest_AABB_edge[i])
 
 
     
     # store in sheet and histogram
     # statistics.draw_histogram(barycenter_origin_distance, 'barycenter distance')
     # statistics.save_Excel(barycenter_origin_distance, 'baryCenter_origin_distance_I')
-    # statistics.draw_histogram(absolute_cosine_similarity, 'cosine similarity')
-    # statistics.save_Excel(absolute_cosine_similarity, 'absolute_cosine_similarity_I')
+    #statistics.draw_histogram(absolute_cosine_similarity, 'cosine similarity')
+    #statistics.save_Excel(absolute_cosine_similarity, 'absolute_cosine_similarity_I')
     statistics.draw_histogram(longest_AABB_edge, 'AABB')
-    statistics.save_Excel(longest_AABB_edge, 'longest_AABB_edge_I')
+    #statistics.save_Excel(longest_AABB_edge, 'longest_AABB_edge_I')
 
     barycenter_origin_distance_Normalized = []
     absolute_cosine_similarity_Normalized = []
@@ -388,9 +388,9 @@ if __name__ == '__main__':
 
 
     for i in range(len(normalized_data)):
-        # barycenter_origin_distance_Normalized.append(util.get_barycenter_origin_distance(normalized_data[i]))
-        # absolute_cosine_similarity_Normalized.append(util.get_Cosine_similarity(normalized_data[i]))
-        longest_AABB_edge_Normalized.append(util.get_longest_AABB_edge(normalized_data[i]))
+        #barycenter_origin_distance_Normalized.append(get_barycenter_origin_distance(normalized_data[i]))
+        #absolute_cosine_similarity_Normalized.append(get_Cosine_similarity(normalized_data[i]))
+        longest_AABB_edge_Normalized.append(get_longest_AABB_edge(normalized_data[i]))
         
 
     # for i in range(len(data)):
@@ -400,10 +400,10 @@ if __name__ == '__main__':
         # print("longest_AABB_edge_normalized: %.5f" %longest_AABB_edge_Normalized[i])
     # statistics.draw_histogram(barycenter_origin_distance_Normalized, 'barycenter distance')
     # statistics.save_Excel(barycenter_origin_distance_Normalized, 'baryCenter_origin_distance_N')
-    # statistics.draw_histogram(absolute_cosine_similarity_Normalized, 'cosine similarity')
-    # statistics.save_Excel(absolute_cosine_similarity_Normalized, 'absolute_cosine_similarity_N')
+    #statistics.draw_histogram(absolute_cosine_similarity_Normalized, 'cosine similarity')
+    #statistics.save_Excel(absolute_cosine_similarity_Normalized, 'absolute_cosine_similarity_N')
     statistics.draw_histogram(longest_AABB_edge_Normalized, 'AABB')
-    statistics.save_Excel(longest_AABB_edge_Normalized, 'longest_AABB_edge_N')
+    #statistics.save_Excel(longest_AABB_edge_Normalized, 'longest_AABB_edge_N')
 
 
     # # get normalized mesh

normalization.py
@@ -15,30 +15,25 @@ def translate_mesh_to_origin(mesh):
 
 # Returns the shape scaled to the unit cube
 def scale_mesh_to_unit(mesh):
-    return scale_mesh(mesh, 1 / np.max(np.linalg.norm(np.asarray(mesh["vertices"]), axis=1)))
+    (xMax, yMax, zMax) = list(np.max(mesh["vertices"], axis=0))
+    (xMin, yMin, zMin) = list(np.min(mesh["vertices"], axis=0))
+
+    maxDist = max(abs(xMax - xMin), abs(yMax - yMin), abs(zMax - zMin))
+
+    return scale_mesh(mesh, 1 / maxDist)
 
 # Align the two largest eigenvectors with the x and y axis
 def align_shape(mesh):
     # Do PCA
     eigenvalues, eigenvectors = compute_PCA(mesh)
-
-    # Get the two largest eigenvectors
-    largest_eigenvector = eigenvectors[:, np.argmax(eigenvalues)]
-    second_largest_eigenvector = eigenvectors[:, np.argsort(eigenvalues)[-2]]
-
-    # Get the two largest eigenvectors' angles with the x and y axis
-    largest_eigenvector_angle = np.arctan2(largest_eigenvector[1], largest_eigenvector[0])
-    second_largest_eigenvector_angle = np.arctan2(second_largest_eigenvector[1], second_largest_eigenvector[0])
-
-    # Get the rotation matrix for the two largest eigenvectors
-    rotation_matrix = np.array([[np.cos(largest_eigenvector_angle), -np.sin(largest_eigenvector_angle), 0],
-                                [np.sin(largest_eigenvector_angle), np.cos(largest_eigenvector_angle), 0],
-                                [0, 0, 1]])
-
+    rot_matrix = np.array(eigenvectors)
+    
     # Rotate the shape
-    mesh["vertices"] = o3d.utility.Vector3dVector(np.dot(rotation_matrix, np.asarray(mesh["vertices"]).T).T)
+    mesh["vertices"] = o3d.utility.Vector3dVector(np.dot(rot_matrix, np.asarray(mesh["vertices"]).T).T)
+
     newAABB = o3d.geometry.AxisAlignedBoundingBox.create_from_points(mesh["vertices"])
     mesh["aabb"] = [newAABB.get_min_bound()[0], newAABB.get_min_bound()[1], newAABB.get_min_bound()[2], newAABB.get_max_bound()[0], newAABB.get_max_bound()[1], newAABB.get_max_bound()[2]]
+    
     return mesh
 
 # Performs the flipping test and mirrors the shape if necessary
@@ -130,17 +125,17 @@ def hole_filling(mesh):
 
 # Applies all the normalization steps to the given mesh
 def normalize(mesh):
-    if mesh['numVerts'] < MINRESAMPLINGTHRESHOLD or mesh['numVerts'] > MAXRESAMPLINGTHRESHOLD:
-        mesh = resampling(mesh)
-    if not is_watertight(mesh):
-        mesh = hole_stitching(mesh)
-    if not is_watertight(mesh):     #especially for "data/LabeledDB_new/Bearing/349.off"
-        mesh["faces"] = mesh["faces"].tolist()
-        mesh = resampling(mesh)
-        mesh = hole_stitching(mesh)
-    mesh = translate_mesh_to_origin(mesh)
-    mesh = align_shape(mesh)
-    mesh = flipping_test(mesh)
+    # if mesh['numVerts'] < MINRESAMPLINGTHRESHOLD or mesh['numVerts'] > MAXRESAMPLINGTHRESHOLD:
+    #     mesh = resampling(mesh)
+    # if not is_watertight(mesh):
+    #     mesh = hole_stitching(mesh)
+    # if not is_watertight(mesh):     #especially for "data/LabeledDB_new/Bearing/349.off"
+    #     mesh["faces"] = mesh["faces"].tolist()
+    #     mesh = resampling(mesh)
+    #     mesh = hole_stitching(mesh)
+    # mesh = translate_mesh_to_origin(mesh)
+    # mesh = align_shape(mesh)
+    # mesh = flipping_test(mesh)
     mesh = scale_mesh_to_unit(mesh)
     return mesh
 

visualizer.py
@@ -6,6 +6,7 @@ import normalization
 import os
 from tqdm import tqdm
 import load_meshes as lm
+import collections.abc
 
 THUMBNAILPATH = os.path.join("data", "normalized")
 THUMBNAILSCALE = 1
@@ -324,7 +325,7 @@ def gen_thumbnails(meshes, outputDir=None):
     
     sunDir = [0.577, -0.577, -0.577]
     renderer.scene.set_lighting(renderer.scene.MED_SHADOWS, sunDir)
-    renderer.scene.show_axes(False)
+    renderer.scene.show_axes(True)
     renderer.scene.show_skybox(False)
     bgColor = [1.0000, 1.0000, 0.9294, 1.0000]
     renderer.scene.set_background(bgColor)
@@ -343,13 +344,17 @@ def gen_thumbnails(meshes, outputDir=None):
     plasticMat.base_anisotropy = 0.0
     plasticMat.shader = "defaultLit"
 
-    geometry = o3d.geometry.TriangleMesh()
     outputPaths = []
 
-    for mesh in tqdm(meshes, desc="Generating thumbnails", ncols=150):
+    if isinstance(meshes, collections.abc.Sequence):
+        tot = len(meshes)
+    else:
+        tot = len(list(meshes.clone())) # Cursor
+    for mesh in tqdm(meshes, desc="Generating thumbnails", ncols=150, total=tot):
         if not "vertices" in mesh:
             mesh = lm.load_mesh(mesh["path"], returnInfoOnly=False)
         
+        geometry = o3d.geometry.TriangleMesh()
         geometry.vertices = o3d.utility.Vector3dVector(mesh["vertices"])
         geometry.triangles = o3d.utility.Vector3iVector(mesh["faces"])
         geometry.compute_triangle_normals()
@@ -358,8 +363,9 @@ def gen_thumbnails(meshes, outputDir=None):
         renderer.scene.add_geometry("Mesh", geometry, plasticMat)
 
         # Camera
-        bounds = renderer.scene.bounding_box
-        center = bounds.get_center()
+        #bounds = renderer.scene.bounding_box
+        #center = bounds.get_center()
+        center = np.asarray([0, 0, 0])
         renderer.setup_camera(60, center, center + [0, 0, 2], [0, 1, 0])
 
         img = renderer.render_to_image()